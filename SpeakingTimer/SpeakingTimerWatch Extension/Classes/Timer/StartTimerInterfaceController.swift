cefa681795aa8dd67803742461c3c7b52dfa8714
Maria <kobinary@gmail.com>
2018-03-23 14:07:28 +0100

    Added PikerInterface to Watch

--- SpeakingTimer/SpeakingTimerWatch Extension/Classes/Timer/StartTimerInterfaceController.swift
+++ SpeakingTimer/SpeakingTimerWatch Extension/Classes/Timer/StartTimerInterfaceController.swift
@@ -16,13 +16,49 @@ import Foundation
 
 class StartTimerInterfaceController: WKInterfaceController {
 
-    @IBOutlet var oneMinButton: WKInterfaceButton!
-    @IBOutlet var twoMinsButton: WKInterfaceButton!
-    @IBOutlet var fiveMinsButton: WKInterfaceButton!
-    @IBOutlet var tenMinsButton: WKInterfaceButton!
+    @IBOutlet var hoursPickerView: WKInterfacePicker!
+    @IBOutlet var minutesPickerView: WKInterfacePicker!
+    @IBOutlet var secondsPickerView: WKInterfacePicker!
+    @IBOutlet var startButton: WKInterfaceButton!
     
+    var pickerViewData = [[""]]
+    var hours : Int = 0
+    var minutes : Int = 0
+    var seconds : Int = 0
+    var viewModel = StartTimerViewModel()
+    
+    // MARK: - All setup methods
+
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
+        self.setupPickerView()
+    }
+    
+    func setupPickerView() {
+        self.getPickerData()
+        self.setupPickerData()
+    }
+    
+    func getPickerData() {
+        self.pickerViewData = viewModel.getPickerValues()
+    }
+    
+    func setupPickerData() {
+        hoursPickerView.setItems(getPickerItems(array: self.pickerViewData[0]))
+        minutesPickerView.setItems(getPickerItems(array: self.pickerViewData[1]))
+        secondsPickerView.setItems(getPickerItems(array: self.pickerViewData[2]))
+    }
+
+    func getPickerItems(array : [String]) -> [WKPickerItem] {
+        var pickerItems : [WKPickerItem] = []
+        
+        for value in array {
+            let pickerItem = WKPickerItem()
+            let correctValue = viewModel.getCorrectValueFrom(stringValue: value)
+            pickerItem.title = correctValue
+            pickerItems.append(pickerItem)
+        }
+        return pickerItems
     }
 
     override func willActivate() {
@@ -33,7 +69,24 @@ class StartTimerInterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
     
+    
+    // MARK: - WKPickerInterface Selection Logic
+    
     override func contextForSegue(withIdentifier segueIdentifier: String) -> Any? {
-        return ["time": Int(segueIdentifier)]
+        return ["time": TimeConversorHelper().transformTimeIntoSeoncds(hours: self.hours, minutes: self.minutes, seconds: self.seconds)]
     }
+    
+    @IBAction func pickerHoursSelectedItemChanged(_ value: Int) {
+        self.hours = value
+    }
+    
+    @IBAction func pickerMinssSelectedItemChanged(_ value: Int) {
+        self.minutes = value
+    }
+    
+    @IBAction func pickerSecsSelectedItemChanged(_ value: Int) {
+        self.seconds = value
+    }
+    
 }
+
